a1260b262dfba366608b8eafe53f64285536bfe7
Sabin Iacob <iacobs@m0n5t3r.info>
2022-05-15 16:20:27 +0100

    feat: split sigmastudio protocol handler common parts into a base module, add TCPIP1701 support

---
+++ sigmadsp/communication/base.py
@@ -0,0 +1,50 @@
+"""This module contains the base classes for SigmaStudio protocol handling."""
+import socketserver
+from dataclasses import dataclass
+from multiprocessing import Pipe
+
+
+@dataclass(frozen=True)
+class WriteRequest:
+    """SigmaStudio requests to write data to the DSP."""
+
+    address: int
+    data: bytes
+
+
+@dataclass(frozen=True)
+class SafeloadRequest:
+    """SigmaStudio requests to write data to the DSP."""
+
+    address: int
+    data: bytes
+
+
+@dataclass(frozen=True)
+class ReadRequest:
+    """SigmaStudio requests to read data from the DSP."""
+
+    address: int
+    length: int
+
+
+@dataclass(frozen=True)
+class ReadResponse:
+    """SigmaStudio is sent a response to its ReadRequest."""
+
+    data: bytes
+
+
+class ThreadedTCPServer(socketserver.ThreadingTCPServer):
+    """The threaded TCP server that is used for communicating with SigmaStudio.
+
+    It will be instantiated by SigmaStudioInterface. General server settings can be adjusted below.
+    """
+
+    allow_reuse_address = True
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the ThreadedTCPServer with a Pipe for communicating with the TCP server worker thread."""
+        self.pipe_end_owner, self.pipe_end_user = Pipe()
+
+        super().__init__(*args, **kwargs)
