165d95226a1afccdd8d6f5b1428f649c454e8379
Sabin Iacob <iacobs@m0n5t3r.info>
2022-05-20 15:17:30 +0100

    fix(adau1701): docstring, call constant fields as Adau1701.field for consistency, use self.read instead of calling the protocol handler

--- sigmadsp/hardware/adau1701.py
+++ sigmadsp/hardware/adau1701.py
@@ -1,10 +1,8 @@
 """This module provides functionality for controlling SigmaDSP hardware, e.g.
 
+- Deploying programs
 - Changing parameter register contents
 - Reading parameter registers
-- Performing soft reset
-
-For this, it uses the SpiHandler module, to interface to the DSP.
 """
 import logging
 from typing import Union
@@ -100,16 +98,16 @@ class Adau1701(Dsp):
         """
         # load up the address and data in safeload registers
         for sd in range(0, count):
-            address_register, data_register = self.SAFELOAD_REGISTERS[sd]
+            address_register, data_register = Adau1701.SAFELOAD_REGISTERS[sd]
             address_bytes = int16_to_bytes(address)
-            data_buf = bytearray(self.SAFELOAD_SD_LENGTH)
+            data_buf = bytearray(Adau1701.SAFELOAD_SD_LENGTH)
 
-            data_buf[1:] = data[sd * self.FIXPOINT_REGISTER_LENGTH : (sd + 1) * self.FIXPOINT_REGISTER_LENGTH]
+            data_buf[1:] = data[sd * Adau1701.FIXPOINT_REGISTER_LENGTH : (sd + 1) * Adau1701.FIXPOINT_REGISTER_LENGTH]
 
             self.write(address_register, address_bytes)
             self.write(data_register, data_buf)
 
-        control_bytes = self.comm_handler.read(self.CONTROL_REGISTER, self.CONTROL_REGISTER_LENGTH)
+        control_bytes = self.read(Adau1701.CONTROL_REGISTER, Adau1701.CONTROL_REGISTER_LENGTH)
         control_reg = bytes_to_int16(control_bytes)
 
         ist_mask = 1 << 5
@@ -117,4 +115,4 @@ class Adau1701(Dsp):
         control_reg |= ist_mask
 
         # start safe load
-        self.write(self.CONTROL_REGISTER, int16_to_bytes(control_reg))
+        self.write(Adau1701.CONTROL_REGISTER, int16_to_bytes(control_reg))
