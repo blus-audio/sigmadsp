60a67de4c60fe198557d907f919885e7cf9d2df8
elagil <elagil@takanome.de>
2023-04-30 08:52:17 +0200

    chore: offer choice to overwrite existing config

---
+++ common_functions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Ask the user a yes/no question. Returns zero for yes, one for no.
+function yes_or_no {
+    while true; do
+        read -p "$* [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;
+            [Nn]*) echo "Aborted" ; return  1 ;;
+        esac
+    done
+}
