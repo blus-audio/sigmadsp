5a7cef340f646b58fc7df8eb25565238ef40b776
elagil <elagil@takanome.de>
2023-05-01 16:06:47 +0200

    fix: env in uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -5,13 +5,13 @@ source ./templates/.env
 source ./common_functions.sh
 
 ### Uninstall happens below
-echo "=== Uninstall $SIGMADSP."
-pipx uninstall $SIGMADSP
+echo "=== Uninstall $SIGMADSP_EXECUTABLE."
+pipx uninstall $SIGMADSP_EXECUTABLE
 
-yes_or_no "Delete configuration folder '$CONFIGURATION_FOLDER?'" && sudo rm -rf $CONFIGURATION_FOLDER
+yes_or_no "Delete configuration folder '$SIGMADSP_CONFIGURATION_FOLDER?'" && sudo rm -rf $SIGMADSP_CONFIGURATION_FOLDER
 
 stop_and_disable_sigmadsp_service
 
 yes_or_no "Delete service configuration '/usr/lib/systemd/system/$SIGMADSP_BACKEND.service'?" && sudo rm /usr/lib/systemd/system/$SIGMADSP_BACKEND.service
 
-echo "=== Uninstalled $SIGMADSP."
+echo "=== Uninstalled $SIGMADSP_EXECUTABLE."
